7f103d25c14e283b094cf48b27a7ae2f1ac03b72
CharishmaKoya <charishma.koya@gmail.com>
2025-07-31 11:55:32 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Hi This is GIT ACTION EXPERIMENT</h1>
+      <h1>version 2 of my project</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
